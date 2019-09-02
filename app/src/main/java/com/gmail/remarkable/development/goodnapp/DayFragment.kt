5992b99fd6f02dc5d32dce211101a575bc816510
M@rlon <development.remarkable@gmail.com>
2019-09-02 10:33:17 +0200

    Add DayFragment class

---
+++ app/src/main/java/com/gmail/remarkable/development/goodnapp/DayFragment.kt
@@ -0,0 +1,24 @@
+package com.gmail.remarkable.development.goodnapp
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class DayFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_day, container, false)
+    }
+}
+
+
+
