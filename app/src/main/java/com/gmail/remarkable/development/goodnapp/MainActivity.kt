2ba0c833050eee03251a5f3d840b090fe8b45cb4
M@rlon <development.remarkable@gmail.com>
2019-09-02 10:34:53 +0200

    Clean MainActivity class

--- app/src/main/java/com/gmail/remarkable/development/goodnapp/MainActivity.kt
+++ app/src/main/java/com/gmail/remarkable/development/goodnapp/MainActivity.kt
@@ -1,28 +1,16 @@
 package com.gmail.remarkable.development.goodnapp
 
 import android.os.Bundle
-import android.widget.ImageButton
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var setTarget: ImageButton
-    lateinit var timeTarget: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Init views
-        setTarget = findViewById(R.id.set_targetTWT_button)
-        timeTarget = findViewById(R.id.time_targetTWT)
-
-        setTarget.setOnClickListener { setTime() }
     }
 
-    // Mock of a method to see the changes to the layout
-    private fun setTime() {
-        timeTarget.text = "06:30"
-    }
+
 }
