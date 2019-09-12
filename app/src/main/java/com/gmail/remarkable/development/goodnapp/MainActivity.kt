2b9faf139157607948a83afa954abd66ed5ee95d
M@rlon <development.remarkable@gmail.com>
2019-09-12 11:30:17 +0200

    Init binding object with setContentView in MainActivity

--- app/src/main/java/com/gmail/remarkable/development/goodnapp/MainActivity.kt
+++ app/src/main/java/com/gmail/remarkable/development/goodnapp/MainActivity.kt
@@ -2,13 +2,16 @@ package com.gmail.remarkable.development.goodnapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.gmail.remarkable.development.goodnapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
     }
 
