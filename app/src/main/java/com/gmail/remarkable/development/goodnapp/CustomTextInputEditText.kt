2bd0f8ee00160a9ae1eb8d0c0f38f333a91f07e7
M@rlon <development.remarkable@gmail.com>
2019-09-17 11:27:21 +0200

    Add CustomTextInputEditText class to handle the focus
    This custom editText overrides onCheckIsTextEditor to prevent soft input
    keyboard auto-open on focus and set the hint on focus change for
    non-touchable devices.

---
+++ app/src/main/java/com/gmail/remarkable/development/goodnapp/CustomTextInputEditText.kt
@@ -0,0 +1,33 @@
+package com.gmail.remarkable.development.goodnapp
+
+import android.content.Context
+import android.graphics.Rect
+import android.util.AttributeSet
+import com.google.android.material.textfield.TextInputEditText
+
+class CustomTextInputEditText : TextInputEditText {
+
+    // Define all 3 constructors since @JvmOverloads doesn't work correctly with defStyleAttr
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    override fun onCheckIsTextEditor(): Boolean {
+        return false
+    }
+
+    override fun onFocusChanged(focused: Boolean, direction: Int, previouslyFocusedRect: Rect?) {
+        super.onFocusChanged(focused, direction, previouslyFocusedRect)
+        val textTemp = this.text.toString()
+        if (focused) {
+            this.hint = "SET"
+        } else {
+            this.hint = ""
+        }
+    }
+}
