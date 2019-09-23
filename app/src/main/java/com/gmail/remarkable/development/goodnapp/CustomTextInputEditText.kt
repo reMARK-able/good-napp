56a720f9333f61729183f2c1aaf692c03b6db091
M@rlon <development.remarkable@gmail.com>
2019-09-23 13:10:09 +0200

    Remove navigation to TimePicker from CustomTextInputEditText

--- app/src/main/java/com/gmail/remarkable/development/goodnapp/CustomTextInputEditText.kt
+++ app/src/main/java/com/gmail/remarkable/development/goodnapp/CustomTextInputEditText.kt
@@ -3,28 +3,15 @@ package com.gmail.remarkable.development.goodnapp
 import android.content.Context
 import android.graphics.Rect
 import android.util.AttributeSet
-import android.view.View
-import androidx.navigation.findNavController
 import com.google.android.material.textfield.TextInputEditText
 
 /***
  * Custom TextInputEditText to handle the focus and data input.
  */
-class CustomTextInputEditText : TextInputEditText, View.OnClickListener {
-
-    init {
-        // Sets the default click listener for data input with navigation.
-        setOnClickListener(this)
-    }
-
-    override fun onClick(v: View) {
-        // Navigation to TimePickerDialogFragment for data input.
-        v.findNavController().navigate(R.id.action_dayFragment_to_timePickerDialogFragment)
-    }
+class CustomTextInputEditText : TextInputEditText {
 
     // Define all 3 constructors since @JvmOverloads doesn't work correctly with defStyleAttr.
     constructor(context: Context) : super(context)
-
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
         context,
