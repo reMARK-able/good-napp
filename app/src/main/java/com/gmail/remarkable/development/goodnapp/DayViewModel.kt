7661f813e91a25c6ba1cd43e548c6d5360ef9a9d
M@rlon <development.remarkable@gmail.com>
2019-09-23 20:42:36 +0200

    Add targetTwt MutableLiveData in viewModel

--- app/src/main/java/com/gmail/remarkable/development/goodnapp/DayViewModel.kt
+++ app/src/main/java/com/gmail/remarkable/development/goodnapp/DayViewModel.kt
@@ -1,12 +1,17 @@
 package com.gmail.remarkable.development.goodnapp
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DayViewModel : ViewModel() {
 
+    val targetTwt = MutableLiveData<String>()
+
     init {
         Log.i("DayViewModel", "DayViewModel is created.")
+
+        targetTwt.value = ""
     }
 
 
