6d3876d81b1c2b2213cf6ee8faa158b938f2fc01
M@rlon <development.remarkable@gmail.com>
2020-02-05 21:52:15 +0100

    Remove databinding from MainActivity

--- app/src/main/java/com/gmail/remarkable/development/goodnapp/MainActivity.kt
+++ app/src/main/java/com/gmail/remarkable/development/goodnapp/MainActivity.kt
@@ -2,16 +2,12 @@ package com.gmail.remarkable.development.goodnapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import com.gmail.remarkable.development.goodnapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
     }
 
