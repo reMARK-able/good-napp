2a53177e9f2f60c72be37dc074a7fcf3bb4dfd36
M@rlon <development.remarkable@gmail.com>
2020-02-05 19:43:08 +0100

    Add first and simple SleepDayAdapter for recyclerview

---
+++ app/src/main/java/com/gmail/remarkable/development/goodnapp/SleepDayAdapter.kt
@@ -0,0 +1,41 @@
+package com.gmail.remarkable.development.goodnapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.gmail.remarkable.development.goodnapp.util.getDateString
+import com.gmail.remarkable.development.goodnapp.util.getStringForRealTWT
+
+class SleepDayAdapter : RecyclerView.Adapter<SleepDayAdapter.ViewHolder>() {
+
+    var data = listOf<SleepDay>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item_day, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = data[position]
+        val holderContext = holder.itemView.context
+        val res = holderContext.resources
+        holder.date.text = getDateString(item.date, holderContext)
+        holder.twt.text = getStringForRealTWT(item.realTWT, res)
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val date: TextView = itemView.findViewById(R.id.item_list_date)
+        val twt: TextView = itemView.findViewById(R.id.item_list_TWT)
+    }
+}
