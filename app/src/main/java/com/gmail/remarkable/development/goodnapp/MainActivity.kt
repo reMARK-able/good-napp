ae697b768d8394739eb2b0084ed0bc7dfbfc020e
M@rlon <development.remarkable@gmail.com>
2020-02-19 09:56:32 +0100

    Change some colors for DarkMode compatibility

--- app/src/main/java/com/gmail/remarkable/development/goodnapp/MainActivity.kt
+++ app/src/main/java/com/gmail/remarkable/development/goodnapp/MainActivity.kt
@@ -8,7 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+//        // Force Dark Mode - just for testing.
+//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     }
 
 
