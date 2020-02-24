753e52087ca0108675278a3f0c49e412073e2e4b
M@rlon <development.remarkable@gmail.com>
2020-02-24 12:29:53 +0100

    Add binding adapters for nightAwake errors

--- app/src/main/java/com/gmail/remarkable/development/goodnapp/util/BindingAdapters.kt
+++ app/src/main/java/com/gmail/remarkable/development/goodnapp/util/BindingAdapters.kt
@@ -3,6 +3,7 @@ package com.gmail.remarkable.development.goodnapp.util
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.gmail.remarkable.development.goodnapp.SleepDay
+import com.google.android.material.textfield.TextInputLayout
 
 @BindingAdapter("dateFormatted")
 fun TextView.setDateFormatted(sleepDay: SleepDay?) {
@@ -37,4 +38,16 @@ fun TextView.setNightAwakeDurationString(sleepDay: SleepDay?, index: Int) {
     sleepDay?.nightAwakes?.getOrNull(index)?.let {
         text = getDurationNapString(it.duration, context.resources)
     }
+}
+
+@BindingAdapter("awakeStartError", "index")
+fun TextInputLayout.setAwakeStartError(sleepDay: SleepDay?, index: Int) {
+    error = if (sleepDay == null) null
+    else validAwakeStart(sleepDay, index, context.resources)
+}
+
+@BindingAdapter("awakeEndError", "index")
+fun TextInputLayout.setAwakeEndError(sleepDay: SleepDay?, index: Int) {
+    error = if (sleepDay == null) null
+    else validAwakeEnd(sleepDay, index, context.resources)
 }
