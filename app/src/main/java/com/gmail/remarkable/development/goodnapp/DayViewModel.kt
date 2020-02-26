bc0f912d88701d90ade3ad8c6e26e34ad53f6bb8
M@rlon <development.remarkable@gmail.com>
2020-02-26 09:55:32 +0100

    Set 'Add button' disabled if nap number is max allowed

--- app/src/main/java/com/gmail/remarkable/development/goodnapp/DayViewModel.kt
+++ app/src/main/java/com/gmail/remarkable/development/goodnapp/DayViewModel.kt
@@ -40,7 +40,10 @@ class DayViewModel(
     val isTodayFabVisible = Transformations.map(days) { days -> isTodayAdded(days) }
 
     // LiveData to set Add nap button enabled or disabled.
-    val isAllDataValid = Transformations.map(mLiveSleepDay) { day -> validateData(day, resources) }
+    val isAllDataValid = Transformations.map(mLiveSleepDay) { day ->
+        if (day.naps.size >= MAX_AWAKES_NUMBER) false
+        else validateData(day, resources)
+    }
 
     // LiveData for outOfBed validation.
     val isOutOfBedValid =
