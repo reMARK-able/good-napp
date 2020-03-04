97fc06dff3f4531a7ce3844bde7c2a3038b3c7b0
M@rlon <development.remarkable@gmail.com>
2020-03-04 22:19:46 +0100

    Add dataBinding

--- app/src/main/java/com/gmail/remarkable/development/goodnapp/CalendarFragment.kt
+++ app/src/main/java/com/gmail/remarkable/development/goodnapp/CalendarFragment.kt
@@ -4,19 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.gmail.remarkable.development.goodnapp.databinding.FragmentCalendarBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class CalendarFragment : Fragment() {
 
+    lateinit var binding: FragmentCalendarBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_calendar, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+        return binding.root
     }
 
 }
