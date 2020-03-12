5ca81480df048514fbf9a2f4bb6fae577487fcc0
M@rlon <development.remarkable@gmail.com>
2020-03-12 12:50:03 +0100

    Add ConfirmationFragment

---
+++ app/src/main/java/com/gmail/remarkable/development/goodnapp/ConfirmationFragment.kt
@@ -0,0 +1,31 @@
+package com.gmail.remarkable.development.goodnapp
+
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+
+/**
+ * Dialog Fragment for delete/clear action confirmation.
+ */
+class ConfirmationFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+
+            val builder = AlertDialog.Builder(it)
+            builder.setMessage("Do you really?")
+                .setPositiveButton("ok",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // Proceed...
+                    })
+                .setNegativeButton("cancel",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // Cancel...
+                    })
+            // Create the AlertDialog object and return it
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+}
